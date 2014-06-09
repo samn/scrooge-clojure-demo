b0eeeccb0d345da20d04a627160bb9fd81280c87
Sam Neubardt <samneubardt@gmail.com>
2014-06-09 18:04:26 -0400

    Add twtr maven & thrift dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,11 @@
 (defproject scrooge-clojure-demo "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "An example of generating Java classes from Thrift IDL with Leiningen"
+  :url "https://github.com/samn/scrooge-clojure-demo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :java-source-paths ["src/java"]
+  ;; warn (but don't fail) on checksum errors since libthrift is missing one :(
+  :repositories [["twitter" {:url "http://maven.twttr.com/" :checksum :warn}]]
+  :dependencies [[com.twitter/scrooge-core "3.9.0"]
+                 [com.twitter/scrooge-runtime "3.9.0"]
+                 [org.apache.thrift/libthrift "0.5.0-1"]])
